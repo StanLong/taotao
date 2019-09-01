c91535faafc7618c7779f9e2954cad9745300809
StanLong <youlinbhz@sina.cn>
2019-09-01 22:31:04 +0800

    缓存同步

--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemContentServiceImpl.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemContentServiceImpl.java
@@ -4,12 +4,14 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.taotao.common.pojo.EasyUIDataGridResult;
 import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.common.utils.HttpClientUtil;
 import com.taotao.mapper.TbContentMapper;
 import com.taotao.pojo.TbContent;
 import com.taotao.service.ItemContentService;
@@ -19,6 +21,13 @@ public class ItemContentServiceImpl implements ItemContentService{
 
 	@Autowired
 	private TbContentMapper contentMapper;
+	
+	@Value("${REST_BASE_URL}")
+	private String REST_BASE_URL;
+	
+	@Value("${REST_CONTENT_SYNC_URL}")
+	private String REST_CONTENT_SYNC_URL;
+	
 
 	@Override
 	public EasyUIDataGridResult getContentList(int page, int rows, long categoryId) {
@@ -36,11 +45,22 @@ public class ItemContentServiceImpl implements ItemContentService{
 		return result;
 	}
 
+	/**
+	 * 新增商品内容分类
+	 */
 	@Override
 	public TaotaoResult insertContent(TbContent content) {
 		content.setCreated(new Date());
 		content.setUpdated(new Date());;
 		contentMapper.insertContent(content);
+		
+		try {
+			//添加缓存同步服务
+			HttpClientUtil.doGet(REST_BASE_URL + REST_CONTENT_SYNC_URL + content.getCategoryId());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		return TaotaoResult.ok();
 	}
 	
