7bf89b60f419155e0f9295432cef8e41aa2bae08
StanLong <youlinbhz@sina.cn>
2019-09-04 22:16:15 +0800

    把mysql里的数据导入 solr

---
+++ taotao-search/src/main/java/com/taotao/search/controller/ItemController.java
@@ -0,0 +1,30 @@
+package com.taotao.search.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.search.service.ItemService;
+
+/**
+ * 导入商品数据到索引库
+ * @author 矢量
+ *
+ * 2019年9月4日
+ */
+@Controller
+@RequestMapping("/manager")
+public class ItemController {
+
+	@Autowired
+	private ItemService itemService;
+	
+	@RequestMapping("/importAll")
+	@ResponseBody
+	public TaotaoResult importAllItems(){
+		return itemService.importAllItems();
+	}
+	
+}
---
+++ taotao-search/src/main/java/com/taotao/search/pojo/Item.java
@@ -0,0 +1,56 @@
+package com.taotao.search.pojo;
+
+public class Item {
+
+	private String id;
+	private String title;
+	private String sell_point;
+	private String price;
+	private String image;
+	private String catrgory_name;
+	private String item_desc;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getSell_point() {
+		return sell_point;
+	}
+	public void setSell_point(String sell_point) {
+		this.sell_point = sell_point;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getCatrgory_name() {
+		return catrgory_name;
+	}
+	public void setCatrgory_name(String catrgory_name) {
+		this.catrgory_name = catrgory_name;
+	}
+	public String getItem_desc() {
+		return item_desc;
+	}
+	public void setItem_desc(String item_desc) {
+		this.item_desc = item_desc;
+	}
+	
+	
+}
