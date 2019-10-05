b3f90ea3e2eefbed7c2b6fc1e184da93e3e7184d
StanLong <youlinbhz@sina.cn>
2019-10-05 17:11:57 +0800

    提交订单

---
+++ taotao-portal/src/main/java/com/taotao/portal/service/impl/OrderServiceImpl.java
@@ -0,0 +1,38 @@
+package com.taotao.portal.service.impl;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.common.utils.HttpClientUtil;
+import com.taotao.common.utils.JsonUtils;
+import com.taotao.portal.pojo.Order;
+import com.taotao.portal.service.OrderService;
+
+/**
+ * 
+ * @author 矢量
+ *
+ * 2019年10月5日
+ */
+@Service
+public class OrderServiceImpl implements OrderService {
+	
+	@Value("${ORDER_BASE_URL}")
+	private String ORDER_BASE_URL;
+	
+	@Value("${ORDER_CREATE_URL}")
+	private String ORDER_CREATE_URL;
+
+	@Override
+	public String createOrder(Order order) {
+		String json = HttpClientUtil.doPostJson(ORDER_BASE_URL + ORDER_CREATE_URL, JsonUtils.objectToJson(order));
+		
+		TaotaoResult result = TaotaoResult.format(json);
+		if(result.getStatus() == 200){
+			return result.getData().toString();
+		}
+		return "";
+	}
+
+}
